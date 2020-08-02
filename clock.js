67ffaa76af3b4def9b24380c4eb03e8947573fe0
d99722 <d99722@gmail.com>
2020-08-02 20:02:59 +0900

    study
    practice

--- clock.js
+++ clock.js
@@ -1,4 +1,20 @@
-const clockContainer = document.querySelector(".js-clock");
-const clockTitle = function init() {};
+const clockContainer = document.querySelector(".js-clock"),
+  clockTitle = clockContainer.querySelector("h1");
+
+function getTime() {
+  const date = new Date();
+  const minutes = date.getMinutes();
+  const hours = date.getHours();
+  const seconds = date.getSeconds();
+  clockTitle.innerText = `${
+    hours < 10 ? `AM 0${hours}` : hours > 12 ? `PM ${hours - 12}` : hours
+  }:${minutes < 10 ? `0${minutes}` : minutes}:${
+    seconds < 10 ? `0${seconds}` : seconds
+  }`;
+}
+
+function init() {
+  setInterval(getTime, 100);
+}
 
 init();
---
+++ todo.js
@@ -0,0 +1,53 @@
+const toDoForm = document.querySelector(".js-toDoForm"),
+  toDoInput = toDoForm.querySelector("input"),
+  toDoList = document.querySelector(".js-toDoList");
+
+const TODOS_LS = "toDos";
+
+const toDos = [];
+
+function saveToDos() {
+  localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
+}
+
+function paintTodo(text) {
+  const li = document.createElement("li");
+  const delBtn = document.createElement("buttom");
+  const span = document.createElement("span");
+  const newId = toDos.length + 1;
+  delBtn.innerText = "😱";
+  span.innerText = text;
+  li.appendChild(delBtn);
+  li.appendChild(span);
+  li.id = newId;
+  toDoList.appendChild(li);
+  const toDoObj = {
+    text: text,
+    id: newId,
+  };
+  toDos.push(toDoObj);
+  saveToDos();
+}
+
+function loadToDos() {
+  const loadedToDos = localStorage.getItem(TODOS_LS);
+  if (loadedToDos !== null) {
+    console.log(loadedToDos);
+    const parsedToDos = JSON.parse(loadedToDos);
+    console.log(parsedToDos);
+  }
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const currentValue = toDoInput.value;
+  paintTodo(currentValue);
+  toDoInput.value = "";
+}
+
+function init() {
+  loadToDos();
+  toDoForm.addEventListener("submit", handleSubmit);
+}
+
+init();
