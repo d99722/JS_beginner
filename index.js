9b0101c6b296500fdf70766d4e53c56289a3dd68
d99722 <d99722@gmail.com>
2020-08-06 22:20:47 +0900

    challenge day4

--- index.js
+++ index.js
@@ -1,46 +1,23 @@
-// <⚠️ DONT DELETE THIS ⚠️>
-const colors = ["#1abc9c", "#3498db", "#9b59b6", "#f39c12", "#e74c3c"];
-// <⚠️ /DONT DELETE THIS ⚠️>
+const title = document.querySelector("body");
+const MAX_WIDTH = window.screen.availWidth;
 
-/*
-✅ The text of the title should change when the mouse is on top of it.
-✅ The text of the title should change when the mouse is leaves it.
-✅ When the window is resized the title should change.
-✅ On right click the title should also change.
-✅ The colors of the title should come from a color from the colors array.
-✅ DO NOT CHANGE .css, or .html files.
-✅ ALL function handlers should be INSIDE of "superEventHandler"
-*/
-
-const title = document.querySelector("h2");
-
-const superEventHandler = {
-  handleResize: function () {
-    title.innerText = "You just resized";
-    title.style.color = colors[0];
-  },
-
-  handleMouseEnter: function () {
-    title.innerText = "The mouse is here!";
-    title.style.color = colors[1];
-  },
-
-  handleMouseLeave: function () {
-    title.innerText = "The mouse is gone!";
-    title.style.color = colors[2];
-  },
-
-  handleRightClick: function () {
-    title.innerText = "That was a right click!";
-    title.style.color = colors[3];
-  },
-};
-
-function init() {
-  window.addEventListener("resize", superEventHandler.handleResize);
-  title.addEventListener("mouseenter", superEventHandler.handleMouseEnter);
-  title.addEventListener("mouseleave", superEventHandler.handleMouseLeave);
-  window.addEventListener("contextmenu", superEventHandler.handleRightClick);
+function handleResize() {
+  const CURRENT_WIDTH = window.innerWidth;
+  if (CURRENT_WIDTH > MAX_WIDTH * 0.75) {
+    remove();
+    title.classList.add("first");
+  } else if (CURRENT_WIDTH > MAX_WIDTH * 0.5) {
+    remove();
+    title.classList.add("second");
+  } else {
+    remove();
+  }
 }
 
-init();
+function remove() {
+  title.classList.remove("first");
+  title.classList.remove("second");
+  title.classList.remove("third");
+}
+
+window.addEventListener("resize", handleResize);
